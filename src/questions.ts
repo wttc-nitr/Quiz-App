732a6d65b53fd39aa103b4e3fb58c5183db27a0a
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-28 18:16:04 +0530

    add dummy Questions array

---
+++ src/questions.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: 'What does HTML stand for?',
+    options: [
+      'Hyper Text Markup Language',
+      'Highly Technical Markup Language',
+      'Hyperlinks and Text Markup Language',
+      'Hyper Tool Markup Language',
+    ],
+    correctAnswer: 'Hyper Text Markup Language',
+  },
+  {
+    title: 'Which language is primarily used for styling web pages?',
+    options: ['HTML', 'Python', 'CSS', 'JavaScript'],
+    correctAnswer: 'CSS',
+  },
+  {
+    title: "In programming, what is a 'bug'?",
+    options: [
+      "A feature that's not documented",
+      'An error in a program',
+      'A type of virus',
+      'An internet crawler',
+    ],
+    correctAnswer: 'An error in a program',
+  },
+  {
+    title: "What is the purpose of a 'for loop' in programming?",
+    options: [
+      'To repeat a block of code a limited number of times',
+      'To style a webpage',
+      'To declare variables',
+      'To create a new function',
+    ],
+    correctAnswer: 'To repeat a block of code a limited number of times',
+  },
+  {
+    title: 'Which symbol is used to denote an ID selector in CSS?',
+    options: ['#', '.', '!', '@'],
+    correctAnswer: '#',
+  },
+];
