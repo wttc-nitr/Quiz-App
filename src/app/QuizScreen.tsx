643a90f9bee48a93a46149832ee1b85fe0c4df94
Animesh Kumar <kumaranimesh097@gmail.com>
2026-03-21 01:44:30 +0530

    lottie animation works, some refactoring

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -8,107 +8,117 @@ import { useQuizContext } from "../providers/QuizProvider";
 import { useEffect } from "react";
 import useTimer from "../hooks/useTimer";
 import LottieView from "lottie-react-native";
-import partyAnimation from "../../assets/party.json";
+// import partyAnimation from "../../assets/party.json";
 
 export default function QuizScreen() {
-  const { question, onNext, score, totalQuestions, questionIndex, bestScore } =
-    useQuizContext();
+    const {
+        question,
+        onNext,
+        score,
+        totalQuestions,
+        questionIndex,
+        bestScore,
+    } = useQuizContext();
 
-  const { time, startTimer, clearTimer } = useTimer(20);
+    const { time, startTimer, clearTimer } = useTimer(20);
 
-  useEffect(() => {
-    startTimer();
+    useEffect(() => {
+        startTimer();
 
-    return () => {
-      clearTimer();
-    };
-  }, [question]);
+        return () => {
+            clearTimer();
+        };
+    }, [question]);
 
-  useEffect(() => {
-    if (time <= 0 && question) onNext();
-  }, [time]);
+    useEffect(() => {
+        if (time <= 0 && question) onNext();
+    }, [time]);
 
-  return (
-    <SafeAreaView style={styles.page}>
-      <View style={styles.container}>
-        {/* Header */}
-        <View>
-          <Text style={styles.title}>
-            {questionIndex >= totalQuestions
-              ? "Result:"
-              : `Question ${questionIndex + 1}/${totalQuestions}`}
-          </Text>
-        </View>
+    return (
+        <SafeAreaView style={styles.page}>
+            <View style={styles.container}>
+                {/* Header */}
+                <View>
+                    <Text style={styles.title}>
+                        {questionIndex >= totalQuestions
+                            ? "Result:"
+                            : `Question ${questionIndex + 1}/${totalQuestions}`}
+                    </Text>
+                </View>
 
-        {/* Body */}
-        <View style={{ gap: 15 }}>
-          {question ? (
-            <>
-              <QuestionCard question={question} />
-              <Text style={styles.time}>{time} sec</Text>
-            </>
-          ) : (
-            <>
-              <LottieView
-                source={partyAnimation}
-                autoPlay
-                style={StyleSheet.absoluteFill}
-                // loop={false}
-              />
-              <Card title="Well done!!">
-                <Text>
-                  Correct answers: {score}/{totalQuestions}
-                </Text>
-                <Text>Best score: {bestScore}</Text>
-              </Card>
-            </>
-          )}
-        </View>
+                {/* Body */}
+                <View style={{ gap: 15 }}>
+                    {question ? (
+                        <>
+                            <QuestionCard question={question} />
+                            <Text style={styles.time}>{time} sec</Text>
+                        </>
+                    ) : (
+                        <>
+                            <Card title="Well done!!">
+                                <LottieView
+                                    source={require("../../assets/party.json")}
+                                    autoPlay
+                                    style={StyleSheet.absoluteFill}
+                                    // loop={false}
+                                />
+                                <Text>
+                                    Correct answers: {score}/{totalQuestions}
+                                </Text>
+                                <Text>Best score: {bestScore}</Text>
+                            </Card>
+                        </>
+                    )}
+                </View>
 
-        {/* Footer */}
-        <CustomButton
-          title="Next"
-          rightIcon={
-            <FontAwesome6 name="arrow-right-long" size={16} color="white" />
-          }
-          onPress={onNext}
-        />
-      </View>
-    </SafeAreaView>
-  );
+                {/* Footer */}
+                <CustomButton
+                    title="Next"
+                    rightIcon={
+                        <FontAwesome6
+                            name="arrow-right-long"
+                            size={16}
+                            color="white"
+                        />
+                    }
+                    onPress={onNext}
+                />
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    backgroundColor: "#FDFEF4",
-  },
-  container: {
-    flex: 1,
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  title: {
-    textAlign: "center",
-    color: "#005055",
-    fontWeight: "500",
-  },
-  time: { textAlign: "center", fontWeight: "bold", color: "#005055" },
-  button: {
-    backgroundColor: "#005055",
-    padding: 20,
-    borderRadius: 100,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "500",
-    letterSpacing: 1.5,
-  },
-  buttonIcon: {
-    position: "absolute",
-    right: 30,
-  },
+    page: {
+        flex: 1,
+        backgroundColor: "#FDFEF4",
+    },
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        padding: 20,
+    },
+    title: {
+        textAlign: "center",
+        color: "#005055",
+        fontWeight: "500",
+    },
+    time: { textAlign: "center", fontWeight: "bold", color: "#005055" },
+    button: {
+        backgroundColor: "#005055",
+        padding: 20,
+        borderRadius: 100,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "500",
+        letterSpacing: 1.5,
+    },
+    buttonIcon: {
+        position: "absolute",
+        right: 30,
+    },
 });
